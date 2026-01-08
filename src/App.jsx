4dcd197c95621208e801709be15a0f19e496a348
ListyaDwiSubagya <listyodwi062@gmail.com>
2026-01-08 20:04:40 +0700

    Initial commit: React agency website

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,16 @@ import Services from './components/Services'
 import OurWork from './components/OurWork'
 import Teams from './components/Teams'
 import ContactUs from './components/ContactUs'
+import {Toaster} from 'react-hot-toast'
+import Footer from './components/Footer'
 
 const App = () => {
 
   const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light')
 
   return (
-    <div className='dark:bg-black relative'>
+    <div className='dark:bg-black relative' >
+      <Toaster/>
       <Navbar theme={theme} setTheme={setTheme}/>
       <Hero/>
       <TrustedBy/>
@@ -20,6 +23,7 @@ const App = () => {
       <OurWork/>
       <Teams/>
       <ContactUs/>
+      <Footer theme={theme} />
     </div>
   )
 }
